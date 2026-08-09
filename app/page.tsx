be88d70ac20ca2be1cb936a1833226d4c50e469a
athletv12 <athletv12@gmail.com>
2026-08-09 12:41:55 +0200

    Add Home component for V12 Coaching App

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>V12 Coaching App</h1>
+      <p>Die App ist erfolgreich online!</p>
+    </main>
+  );
+}
